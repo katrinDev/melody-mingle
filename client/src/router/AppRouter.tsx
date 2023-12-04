f413d18f8c2752777d97be1ce2156e12fe46daf9
katrinDev <polozkovak8@gmail.com>
2023-12-04 03:27:29 +0300

    create first half of about page interface

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -3,8 +3,9 @@ import SignIn from "../pages/authPages/SignIn";
 import SignUp from "../pages/authPages/SignUp";
 import { useContext } from "react";
 import { Context } from "../main";
-import About from "../pages/About";
+import About from "../pages/about/About";
 import { observer } from "mobx-react-lite";
+import BasicProfileLayout from "../pages/layouts/BasicProfileLayout";
 
 const AppRouter: React.FC = observer(() => {
   const { userStore } = useContext(Context);
@@ -12,6 +13,7 @@ const AppRouter: React.FC = observer(() => {
   return userStore.isAuth ? (
     <Routes>
       <Route path="/about" element={<About />} />
+      <Route path="/my-profile" element={<BasicProfileLayout />} />
       <Route path="*" element={<Navigate to="/about" />} />
     </Routes>
   ) : (
